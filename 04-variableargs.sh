b5862a5bfcb1b789ed9844d1d2ee7c4218eb19f0
Nagaraj <thotinagaraj8@gmail.com>
2024-02-13 20:27:22 +0530

    3rd commit

---
+++ 04-variableargs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSION1=$1
+PERSION2=$2
+
+echo "$PERSION1: hi $PERSION2, how are you"
+echo "$PERSION2: i am vary good $PERSION1, how are you"
+echo "$PERSION1: i am good $PERSION2"
