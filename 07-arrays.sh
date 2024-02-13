639c193939c8ecbaf65aaef10b85338181fbeef9
Nagaraj <thotinagaraj8@gmail.com>
2024-02-13 22:14:58 +0530

    3rd commit

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -2,8 +2,8 @@
 
 FRUITS=("apple" "banana" "orange")
 
-echo "first value is : $(FRUITS[0])"
-echo "second value is : $(FRUITS[1])"
-echo "thired value is : $(FRUITS[2])"
-echo "fourth value is : $(FRUITS[3])"
-echo "all fruits is : $(FRUITS[@])"
+echo "first value is : ${FRUITS[0]}"
+echo "second value is : ${FRUITS[1]}"
+echo "thired value is : ${FRUITS[2]}"
+echo "fourth value is : ${FRUITS[3]}"
+echo "all fruits is : ${FRUITS[@]}"
