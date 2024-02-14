b7f7ba1b2b6f0c078d9befd5511d2bcc54242970
Nagaraj <thotinagaraj8@gmail.com>
2024-02-14 20:21:20 +0530

    3rd commit

--- 11-log.sh
+++ 11-log.sh
@@ -17,9 +17,9 @@ VALIDATE(){
     if [ $1 -ne 0 ]
 
     then
-        echo -e "ERROR::$2...  is $Y FAILED $N"
+        echo -e "ERROR::$2...  is $R FAILED $N"
     else
-        echo -e "$2...is $G SUCCESS $N"
+        echo -e "$2...is $Y SUCCESS $N"
     fi
 
 }
