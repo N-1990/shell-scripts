2d22589b887d1d7af1c70998f062840236bc4251
Nagaraj <thotinagaraj8@gmail.com>
2024-02-14 18:06:33 +0530

    commit

---
+++ 10-functionsvalidation.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]
+
+    then
+        echo "ERROR::$2...  is FAILED"
+    else
+        echo "$2...is SUCCESS"
+    fi
+
+}
+
+if [ $ID -ne 0 ]
+
+then
+    echo "ERROR:: please run the script with root access"
+    exit 1
+else
+    echo "you are in root user"
+fi
+
+yum install mysql -y
+
+VALIDATE @? "MySQL installed"
+
+yum install git -y
+
+VALIDATE @? "GIT installed"
