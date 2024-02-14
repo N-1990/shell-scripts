934ce9a5ece915a1adc14267860ca71d4b6da331
Nagaraj <thotinagaraj8@gmail.com>
2024-02-14 20:07:31 +0530

    3rd commit

--- 11-log.sh
+++ 11-log.sh
@@ -5,14 +5,19 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 VALIDATE(){
 
     if [ $1 -ne 0 ]
 
     then
-        echo "ERROR::$2...  is FAILED"
+        echo -e "ERROR::$2...  is $R FAILED"
     else
-        echo "$2...is SUCCESS"
+        echo -e "$2...is $G SUCCESS"
     fi
 
 }
@@ -20,7 +25,7 @@ VALIDATE(){
 if [ $ID -ne 0 ]
 
 then
-    echo "ERROR:: please run the script with root access"
+    echo -e "$R ERROR:: please run the script with root access"
     exit 1
 else
     echo "you are in root user"
