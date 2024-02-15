a5b4b25808ea5b57fcf5f07094d88f3c46fe2e94
Nagaraj <thotinagaraj8@gmail.com>
2024-02-15 12:00:51 +0530

    3rd commit

---
+++ 13-installedpackageusing loops.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ..is $R FAILED $N"
+    else
+        echo -e "$2 ..is $G SUCCESS $N"
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run the script with root user access $N"
+    exit 1
+else
+    echo "your are in root user"    
+fi
+
+for package in $@
+do
+    yum list installed $package
+    if [ $? -ne 0 ]
+    then 
+        yum installed $package -y
+        VALIDATE $? "Istallation of $package"
+    else
+        echo -e "$package is already installed ... $Y SKIP $N"
+    fi
+done
